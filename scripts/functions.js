8c400f6d90737762296c217447b3d1df6ebdb791
Alexander Desktop <alexanderfarlinger@gmail.com>
2021-11-28 14:06:56 -0800

    So, I tend to only commit once I've finished one day's worth of work, meaning that if an assignment takes more than one day, or if an assignment builds off of the git repo of another assignment, then I'll do more commits. However, since I finished all of the work for the assignment in one day, I only make one commit. Its my personal work philosiphy that the work this assignment needed was not enough to require multiple commits, but I'll accept the mark loss anyways because there's nothing I can do about it now that I wrote all the code besides faking it (and thats just unnecesary work). This commit is for the entire assignment, and I'm sticking with it.

---
+++ scripts/functions.js
@@ -0,0 +1,175 @@
+/* This file is where all your work should go for Assignment 2.  Please remove this comment. */
+
+
+//cleanUpPage works for all pages
+function cleanUpPage() {
+    $main.children().remove();
+}
+
+function createSingleIndex(object) {
+    let contact = document.createElement("a");
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+
+    contact.href = "page3.html";
+    div.classList.add("contact");
+    p.innerHTML = object.name;
+
+    div.append(p);
+    contact.append(div);
+    return contact;
+}
+
+function renderIndex() {
+    for (let i = 0;i < contactList.length;i++) {
+        console.log("method complete");
+        $main.append(createSingleIndex(contactList[i]));
+    } 
+}
+
+function renderView(object) {
+    let contact = document.createElement("div");
+    let contactName = document.createElement("div");
+    let profilePic = document.createElement("img");
+    let contactEmail = document.createElement("div");
+    let contactPhone = document.createElement("div");
+    let contactAddress = document.createElement("div");
+    let buttons = document.createElement("div");
+    let editButton = document.createElement("button");
+    let closeButton = document.createElement("button");
+
+    contact.classList.add("contactinfo");
+    contactName.classList.add("contactname");
+    profilePic.classList.add("profilepic");
+    contactEmail.classList.add("contactemail");
+    contactPhone.classList.add("contactphone");
+    contactAddress.classList.add("contentaddress");
+    buttons.classList.add("buttons");
+    editButton.classList.add("button");
+    editButton.classList.add("edit");
+    closeButton.classList.add("button");
+    closeButton.classList.add("close");
+
+    profilePic.src = "img/profile.jpg";
+    profilePic.alt = "Profile picture";
+    editButton.value = "Edit"
+    closeButton.value = "Close";
+
+    contactName.innerHTML = object.name;
+    contactEmail.innerHTML = object.email;
+    contactPhone.innerHTML = object.phone;
+    contactAddress.innerHTML = object.address;
+    editButton.innerHTML = "Edit";
+    closeButton.innerHTML = "Close";
+
+    contactName.append(profilePic);
+    buttons.append(editButton);
+    buttons.append(closeButton);
+    contact.append(contactName);
+    contact.append(contactEmail);
+    contact.append(contactPhone);
+    contact.append(contactAddress);
+    contact.append(buttons);
+
+    console.log(contact);
+    $main.append(contact);
+}
+
+function renderCreate() {
+    let contactEdit = document.createElement("div");
+    let contactImg = document.createElement("div");
+    let profilePic = document.createElement("img");
+    let formDiv = document.createElement("div");
+    let form = document.createElement("form");
+
+    let inputContainer1 = document.createElement("div");
+    let contactName = document.createElement("input");
+    let extraField1 = document.createElement("button");
+
+    let inputContainer2 = document.createElement("div");
+    let contactPhone = document.createElement("input");
+    let extraField2 = document.createElement("button");
+
+    let inputContainer3 = document.createElement("div");
+    let contactAddress = document.createElement("input");
+    let extraField3 = document.createElement("button");
+
+    let inputContainer4 = document.createElement("div");
+    let contactEmail = document.createElement("input");
+    let extraField4 = document.createElement("button");
+
+    let inputContainer5 = document.createElement("div");
+    let submitButton = document.createElement("button");
+    let resetButton = document.createElement("button");
+
+    contactEdit.classList.add("contactedit");
+    contactImg.classList.add("contactimg");
+    profilePic.classList.add("profilepic");
+    formDiv.classList.add("form");
+
+    inputContainer1.classList.add("inputcontainer");
+    contactName.id = "contactname";
+    contactName.placeholder = "Contact Name";
+    extraField1.classList.add("extrafield");
+    extraField1.id = "extranamefield";
+
+    inputContainer2.classList.add("inputcontainer");
+    contactPhone.id = "contactphone";
+    contactPhone.placeholder = "Contact Phone";
+    extraField2.classList.add("extrafield");
+    extraField2.id = "extraphonefield";
+
+    inputContainer3.classList.add("inoutcontainer");
+    contactAddress.id = "contactaddress";
+    contactAddress.placeholder = "Contact Address";
+    extraField3.classList.add("extrafield");
+    extraField3.id = "extraaddressfield";
+
+    inputContainer4.classList.add("inputcontainer");
+    contactEmail.id = "contactemail";
+    contactEmail.placeholder = "Contact Email";
+    extraField4.classList.add("extrafield");
+    extraField4.id = "extraemailfield";
+
+    inputContainer5.classList.add("inputcontainer");
+    submitButton.classList.add("button");
+    submitButton.classList.add("save");
+    submitButton.id = "savecontact";
+    resetButton.classList.add("button");
+    resetButton.classList.add("cancel")
+    resetButton.id = "cancel";
+
+    profilePic.src = "img/profile.jpg";
+    profilePic.alt = "Profile picture";
+    submitButton.type = "submit";
+    resetButton.type = "reset";
+
+    submitButton.innerHTML = "Save";
+    resetButton.innerHTML = "Cancel";
+
+    inputContainer1.append(contactName);
+    inputContainer1.append(extraField1);
+    inputContainer2.append(contactPhone);
+    inputContainer2.append(extraField2);
+    inputContainer3.append(contactAddress);
+    inputContainer3.append(extraField3);
+    inputContainer4.append(contactEmail);
+    inputContainer4.append(extraField4);
+    inputContainer5.append(submitButton);
+    inputContainer5.append(resetButton);
+
+    form.append(inputContainer1);
+    form.append(inputContainer2);
+    form.append(inputContainer3);
+    form.append(inputContainer4);
+    form.append(inputContainer5);
+
+    contactImg.append(profilePic);
+    formDiv.append(form);
+
+    contactEdit.append(contactImg);
+    contactEdit.append(formDiv);
+    $main.append(contactEdit);
+}
+
+console.log("functions loaded");
---
+++ scripts/variables.js
@@ -0,0 +1,23 @@
+const $main = $(".main");
+
+const contactList = [
+    {
+        name: "Roberta Dobbs",
+        phone: "778-555-1234",
+        address: "101 Main St, Anytown, USA",
+        email: "subgenius@slack.example.com",
+      }, 
+      {
+        name: "Bugs Bunny",
+        phone: "123-867-5309",
+        address: "Warner Brothers Animation Lot",
+        email: "whatsup@doc.example.com",
+      },
+      {
+          name: "Amakusa Shirou Tokisada",
+          phone: "111-222-3333",
+          address: "Your nearest church",
+          email: "semikusa@hotmail.com",
+      }
+]
+console.log("variables loaded");
